c997e3974ee9fe0dce66bce6cec37854141b38ba
mush42 <musharraf.omer@gmail.com>
2022-02-12 19:58:37 +0200

    Better discovery of apks. Include the ware apk. Changed the focus-actionable sound.

--- build.gh-actions.sh
+++ build.gh-actions.sh
@@ -104,5 +104,9 @@ if [[ $BUILD_EXIT_CODE -eq 0 ]]; then
   log
 fi
 
+RELEASE_APKS_DIR=release_apks
+mkdir ./${RELEASE_APKS_DIR}
+mv ./build/outputs/apk/phone/release/talkback-phone-release-unsigned.apk ./${RELEASE_APKS_DIR}/TalkBack-phone.apk
+mv ./build/outputs/apk/wear/release/talkback-wear-release-unsigned.apk ./${RELEASE_APKS_DIR}/TalkBack-wear.apk
 
 exit $BUILD_EXIT_CODE   ### This should be the last line in this file
